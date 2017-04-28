7a22ca51329a79d4aceeb9022dd2a2ee42508fbb
AnLorXen <anlorxen@cox.net>
2017-04-28 14:53:33 -0700

    Add files via upload

---
+++ js/utility.js
@@ -0,0 +1,188 @@
+﻿
+AnLorXen.utility = {
+  "palettes": [
+      {
+        "name": "BlueOcean", // 0
+        "palette": [
+            "#185B7B", "#6375CF", "#2D2B31", "#7C7F7F", "#505594",
+            "#72779A", "#504D65", "#437BBD", "#0554A9", "#203B6D",
+            "#565C5C", "#0271A0", "#787CB9", "#5E7B8F", "#255A98",
+            "#2B4B71", "#6E656D", "#5764B5", "#837BC4", "#383937",
+            "#3F416E", "#494252", "#4069C6", "#3A59A9", "#5B7DBD"
+        ]
+      },
+      {
+        "name": "Common", // 1
+        "palette": [
+            "#C8427C", "#73DA4C", "#6FD3D0", "#D6943C", "#737BD6",
+            "#374328", "#412A48", "#C8D382", "#D64B2A", "#C3917A",
+            "#BC5CDF", "#CB91B6", "#80401E", "#549137", "#D2D43D",
+            "#71DA9B", "#7AA4D2", "#5C8B70", "#6E4388", "#C9CDC4",
+            "#817B33", "#55657B", "#CB4E59", "#D256B6", "#673036"
+        ]
+      },
+      {
+        "name": "Fluorescent", // 2
+        "palette": [
+            "#48E7F8", "#FEE411", "#FFA5DF", "#15E835", "#65FEB7",
+            "#A1CE56", "#E8B747", "#A6F213", "#69F47A", "#35F4DE",
+            "#E1F246", "#6BD72F", "#95EF89", "#E1C12F", "#F5A9F8",
+            "#36DA9D", "#86F767", "#78F43A", "#6FD66A", "#A3E574",
+            "#8EDF27", "#21E9C7", "#57D55E", "#E1C041", "#20DBE7"
+        ]
+      },
+      {
+        "name": "GreenMint", // 3
+        "palette": [
+            "#6D9724", "#1CF5B0", "#CDD7A7", "#357D59", "#A7F746",
+            "#BDFC9B", "#13A954", "#928E67", "#ACB665", "#4DF587",
+            "#6BB28B", "#289014", "#B6F26A", "#59721A", "#118038",
+            "#74C70C", "#98AE8D", "#9DC250", "#CBE488", "#61A605",
+            "#37C37D", "#D5EFA7", "#9EF67C", "#54EE9F", "#13821D"
+        ]
+      },
+      {
+        "name": "IceCube", // 4
+        "palette": [
+            "#41F3CE", "#596453", "#D0D4C1", "#0A9398", "#7CF5FD",
+            "#76B795", "#71918F", "#0BBF95", "#9FD4D1", "#417A66",
+            "#B6E9C8", "#83E1B1", "#9EA38C", "#03B0A3", "#D6FAEF",
+            "#37B7C3", "#84F9E8", "#9AB4AD", "#13D6E3", "#1E897E",
+            "#7DF6D3", "#5B9B7D", "#436758", "#7AE1EB", "#01B2BF"
+        ]
+      },
+      {
+        "name": "IndigoNight", // 5
+        "palette": [
+            "#7D4A9C", "#D263F8", "#DCA9F7", "#7E73F6", "#675C8B",
+            "#E67AE5", "#C36CB5", "#9384DC", "#D43CD1", "#764FCB",
+            "#953A8D", "#AF70F4", "#8B5C8F", "#D388FB", "#AC80C9",
+            "#6262D2", "#F198F2", "#734FB2", "#D65AD0", "#E380D2",
+            "#B553DB", "#9D53A0", "#8677C4", "#7757A7", "#CA7CBD"
+        ]
+      },
+      {
+        "name": "Intense", // 6
+        "palette": [
+            "#FB8D1A", "#7456DF", "#62C9C2", "#890540", "#535D00",
+            "#093948", "#ADB7FC", "#F152C6", "#7D2E02", "#FC6F67",
+            "#C5BE6B", "#444A9D", "#B59CB7", "#EB4A0E", "#F37EB8",
+            "#DAB931", "#1D6D2B", "#CA072F", "#4B3C12", "#6DB6E0",
+            "#3F3566", "#BF88FD", "#299DAF", "#2C5976", "#E4199C"
+        ]
+      },
+      {
+        "name": "Pastel", // 7
+        "palette": [
+            "#9CF9B1", "#E7B5DB", "#CCA965", "#81B7D4", "#C8E4CA",
+            "#FAA893", "#BFD675", "#F9D1C6", "#6DCAB0", "#B4BF87",
+            "#F0D269", "#D8E0E9", "#F1D29E", "#D7FBB5", "#D0A37E",
+            "#C7ACB3", "#77F2C8", "#91BDA9", "#F3D1E2", "#A7DA8A",
+            "#B1B9C2", "#C8B7DD", "#ABB7D3", "#9CD0EC", "#94EEA7"
+        ]
+      },
+      {
+        "name": "PurpleWine", // 8
+        "palette": [
+            "#5E1B59", "#7F6562", "#AC27A8", "#BE3463", "#31191D",
+            "#B33087", "#724461", "#791E47", "#93055B", "#310B28",
+            "#974E7F", "#624044", "#A24969", "#622436", "#BE1B74",
+            "#3B061A", "#690E66", "#9B1F4D", "#974283", "#511446",
+            "#3D1B2E", "#AC2876", "#352329", "#723D63", "#AF0B67"
+        ]
+      },
+      {
+        "name": "RedRose", // 9
+        "palette": [
+            "#A44B50", "#FC4624", "#F2996A", "#D30C5A", "#FC6E6F",
+            "#C6212B", "#CF652E", "#FDA89D", "#AF7864", "#964B2C",
+            "#F00947", "#F76647", "#F05F7F", "#BD443C", "#CB4056",
+            "#EF858C", "#CB9E7E", "#B7744D", "#925444", "#ED7D5E",
+            "#D06475", "#D9361A", "#F44872", "#FA9C88", "#F7637C"
+        ]
+      },
+      {
+        "name": "Spectral", // 10
+        "palette": [
+            "#5E5D22", "#845B80", "#135A5A", "#854239", "#433139",
+            "#302D18", "#304960", "#336F50", "#77646B", "#0A3130",
+            "#7D5930", "#4A370D", "#5C311B", "#6D5349", "#537167",
+            "#544259", "#5E663E", "#653A40", "#874966", "#2F322C",
+            "#333A15", "#4F6B53", "#2C5869", "#5C2E24", "#8D5C7F"
+        ]
+      },
+      {
+        "name": "Tarnish", // 11
+        "palette": [
+            "#556250", "#D2B1B0", "#ACAE83", "#B1886D", "#7C5D56",
+            "#95908C", "#7E6F51", "#6D6A6A", "#A2AF9B", "#9D7C75",
+            "#D5A989", "#7C8A79", "#A5957A", "#595146", "#BFB099",
+            "#AE9093", "#877C6C", "#C9A394", "#8C8A65", "#64573C",
+            "#B9B0AA", "#C5AF89", "#6C6356", "#5E4F4D", "#C3A4A4"
+        ]
+      }
+  ],
+
+  "mouse": function (_ele) {
+    var mouse = {
+      "x": 0,
+      "y": 0, 
+      "evt": null
+    };
+
+    _ele.addEventListener("mousemove", function (_event) {
+      var x, y;
+      if (_event.pageX || _event.pageY) {
+        x = _event.pageX;
+        y = _event.pageY;
+      } else {
+        x = _event.clientX + document.body.scrollLeft
+          + document.documentElement.scrollLeft;
+        y = _event.clientY + document.body.scrollTop
+          + document.documentElement.scrollTop;
+      }
+      x -= _ele.offsetLeft;
+      y -= _ele.offsetTop;
+
+      mouse.x = x;
+      mouse.y = y;
+      mouse.evt = _event;      
+    }, false);
+    return mouse;
+  }
+};
+
+
+
+
+//window.utils.captureMouse = function (element) {
+//  var mouse = { x: 0, y: 0, evt: null };
+//  var body_scrollLeft = document.body.scrollLeft;
+//  var element_scrollLeft = document.documentElement.scrollLeft;
+//  var body_scrollTop = document.body.scrollTop;
+//  var element_scrollTop = document.documentElement.scrollTop;
+//  var offsetLeft = element.offsetLeft;
+//  var offsetTop = element.offsetTop;
+
+//  element.addEventListener("mousemove", function (event) {
+//    var x, y;
+
+//    if (event.pageX || event.pageY) {
+//      x = event.pageX;
+//      y = event.pageY;
+//    } else {
+//      x = event.clientX + body_scrollLeft + element_scrollLeft;
+//      y = event.clientY + body_scrollTop + element_scrollTop;
+//    }
+//    x -= offsetLeft;
+//    y -= offsetTop;
+
+//    mouse.x = x;
+//    mouse.y = y;
+//    mouse.evt = event;
+//  }, false);
+
+//  return mouse;
+//};
+
+
